38eb9348071adf77cb8827ff8bb29a81e2be3929
jonaskaufmann <jonas.kaufmann@sunrise.ch>
2021-08-04 15:53:57 +0200

    Number Guess

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,46 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	System.out.println("Github Test");
-    }
+	public static void main(String[] args) {
+		System.out.println("Github Test");
+
+		int number = (int) (Math.random()*(30));
+		System.out.println(number);
+
+		int tries = 0;
+		while (tries < 9) {
+
+			if (tries < 10) {
+				int left = 9 - tries;
+				System.out.println("Du hast " + left + "Versuche verbleibend");
+				for (int t = 0; t <= 2; t++) {
+					System.out.println("Versuchen Sie die Zahl herauszufinden.");
+					Scanner scanner = new Scanner(System.in);
+					int versuch = scanner.nextInt();
+					System.out.println(versuch);
+					if (versuch == number) {
+						System.out.println("Du hast es geschafft!");
+						tries = 10;
+						t = 3;
+					} else if (versuch > number) {
+						System.out.println("Die gesuchte Zahl ist kleiner.");
+					} else {
+						System.out.println("Die gesuchte Zahl ist grösser.");
+					}
+					tries++;
+				}
+
+
+			}
+
+		}
+		if (tries == 9) {
+			System.out.println("Sie haben verloren.");
+		} else if (tries == 10) {
+			System.out.println("Sie haben gewonnen.");
+		}
+	}
 }
